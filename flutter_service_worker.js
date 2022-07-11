ceddadab9c4e82293b89a3b9990a7869092855c6
Bryan Alexander Vazquez Hernandez <60861327+BryanVazquezHdez@users.noreply.github.com>
2022-07-10 21:12:56 -0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "10cedc975dcfc8dea13d63d1520acb22",
-"index.html": "fd32821ed9f469d687f80e3013a83796",
-"/": "fd32821ed9f469d687f80e3013a83796",
-"main.dart.js": "3c3db79435e67f8ee0c030f4dc87136e",
+"index.html": "70ce6e2c30cceb5032d609692299c378",
+"/": "70ce6e2c30cceb5032d609692299c378",
+"main.dart.js": "b806c1cc44f1cb4ddf5ee8b3e90ee853",
 "favicon.png": "7aefa01752d1d8764c8cc0a1297741da",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
